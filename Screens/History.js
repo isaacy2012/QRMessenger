1ead7220729b5a0636f0fd26b2fedd7696e19c06
isaacy2012 <50868057+isaacy2012@users.noreply.github.com>
2021-06-03 20:17:37 +1200

    Changed History Screen such that "Today, Yesterday..." section header only shows if there is items in that section

--- Screens/History.js
+++ Screens/History.js
@@ -11,9 +11,23 @@ import * as SQLite from "expo-sqlite";
 import {useEffect, useState} from "react";
 import {shouldThrowAnErrorOutsideOfExpo} from "expo/build/environment/validatorState";
 
+/**
+ * Today
+ * @returns {Date}
+ */
+function todayDate() {
+    const ret = new Date();
+    ret.setHours(0,0,0,0);
+    return ret;
+}
 
+/**
+ * Checks if a day is today
+ * @param date
+ * @returns {boolean}
+ */
 function isToday(date) {
-    const today = new Date();
+    const today = todayDate();
     return date.getDate() === today.getDate() &&
         date.getMonth() === today.getMonth() &&
         date.getFullYear() === today.getFullYear();
@@ -21,15 +35,14 @@ function isToday(date) {
 
 /**
  * Days between
- * @param a first date
- * @param b second date
+ * @param date first date
  * @returns {number}
  */
-function daysBetween(a, b) {
+function daysSince(date) {
     // One day in milliseconds
     const oneDay = 1000 * 60 * 60 * 24;
     // Calculating the signed time difference between two dates
-    const diffInTime = b.getTime() - a.getTime();
+    const diffInTime = todayDate().getTime() - date.getTime();
     // Difference in days
     return Math.round(diffInTime / oneDay);
 }
@@ -61,20 +74,7 @@ export default function History({navigation, route}) {
     const [todayItems, setTodayItems] = useState([]);
     const [yesterdayItems, setYesterdayItems] = useState([]);
     const [olderItems, setOlderItems] = useState([]);
-    const HistorySectionViewData = [
-        {
-            title: "Today",
-            data: todayItems
-        },
-        {
-            title: "Yesterday",
-            data: yesterdayItems
-        },
-        {
-            title: "Older",
-            data: olderItems
-        },
-    ]
+    const [HistorySVData, setHistorySVData] = useState([]);
 
     function clearTable() {
         db.transaction(tx => {
@@ -85,49 +85,70 @@ export default function History({navigation, route}) {
             setTodayItems([]);
             setYesterdayItems([]);
             setOlderItems([]);
-            console.log(HistorySectionViewData);
+            console.log(HistorySVData);
         });
+    }
+
+    /**
+     * Refresh the section list from the data in the database
+     */
+    function refreshSectionList() {
+        db.transaction(tx => {
+                tx.executeSql(
+                    'create table if not exists items (id integer primary key not null, date text, value text);'
+                );
+                tx.executeSql('select * from items order by id asc', [], (_, {rows}) => {
+                        //console.log(rows);
+                        for (let i = 0; i < rows.length; i++) {
+                            let item = rows.item(i);
+                            if (isToday(new Date(item.date))) {
+                                if (todayItems.some(el => compareItem(el, item)) === false) {
+                                    todayItems.unshift(item);
+                                }
+                            } else if (daysSince(new Date(item.date)) === 1) {
+                                if (yesterdayItems.some(el => compareItem(el, item)) === false) {
+                                    yesterdayItems.unshift(item);
+                                }
+                            } else {
+                                console.log("diff was : " + daysSince(new Date(item.date)));
+                                console.log("since todayDate was " + todayDate());
+                                if (olderItems.some(el => compareItem(el, item)) === false) {
+                                    olderItems.unshift(item);
+                                }
+                            }
+                            setTodayItems(todayItems);
+                            setYesterdayItems(yesterdayItems);
+                            setOlderItems(olderItems);
+                            let tempHistorySVData = [];
+                            if (todayItems.length > 0) {
+                                tempHistorySVData.push({title:"Today", data:todayItems});
+                            }
+                            if (yesterdayItems.length > 0) {
+                                tempHistorySVData.push({title:"Yesterday", data:yesterdayItems});
+                            }
+                            if (olderItems.length > 0) {
+                                tempHistorySVData.push({title:"Older", data:olderItems});
+                            }
+                            setHistorySVData(tempHistorySVData);
+                            console.log(tempHistorySVData);
+                        }
+                    }
+                );
+            }
+            , undefined
+            , function after() {
+                console.log(HistorySVData);
+                console.log("updating");
+                setChanged(changed => !changed);
+            }
+        );
     }
 
     useFocusEffect(
         React.useCallback(() => {
             // Do something when the screen is focused
             console.log("here");
-            db.transaction(tx => {
-                    tx.executeSql(
-                        'create table if not exists items (id integer primary key not null, date text, value text);'
-                    );
-                    tx.executeSql('select * from items order by id asc', [], (_, {rows}) => {
-                            //console.log(rows);
-                            for (let i = 0; i < rows.length; i++) {
-                                let item = rows.item(i);
-                                if (isToday(new Date(item.date))) {
-                                    if (todayItems.some(el => compareItem(el, item)) === false) {
-                                        todayItems.unshift(item);
-                                    }
-                                } else if (daysBetween(new Date(item.date), new Date()) === 1) {
-                                    if (yesterdayItems.some(el => compareItem(el, item)) === false) {
-                                        yesterdayItems.unshift(item);
-                                    }
-                                } else {
-                                    if (olderItems.some(el => compareItem(el, item)) === false) {
-                                        olderItems.unshift(item);
-                                    }
-                                }
-                                setTodayItems(todayItems);
-                                setYesterdayItems(yesterdayItems);
-                                setOlderItems(olderItems);
-                            }
-                        }
-                    );
-                }
-                , undefined
-                , function after() {
-                    console.log(HistorySectionViewData);
-                    console.log("updating");
-                    setChanged(changed => !changed);
-                }
-            );
+            refreshSectionList();
             return () => {
                 // Do something when the screen is unfocused
                 // Useful for cleanup functions
@@ -184,11 +205,11 @@ export default function History({navigation, route}) {
     }
 
 
-    console.log("drawn");
+    //console.log("drawn");
     return (
         <SafeAreaView style={styles(colors).app}>
             <SectionList
-                sections={HistorySectionViewData}
+                sections={HistorySVData}
                 extraData={changed}
                 ListHeaderComponent={() => <Title/>}
                 // keyExtractor={(item, index) => item + index}
